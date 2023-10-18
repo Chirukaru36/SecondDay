77a970bc0b0cd819e214d32696e83ed349640a01
Chirukaru36 <kpoojashree470@gmail.com>
2023-10-18 09:12:03 +0530

    Add project files.

---
+++ CSharpBasics/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace CSharpBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entwer the first number");
+            int firstno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int secondno = Convert.ToInt32(Console.ReadLine());
+            int ans = firstno + secondno;
+            Console.WriteLine($"answer of {firstno} + {secondno} = {ans}");
+            Console.ReadLine();
+
+        }
+    
+    }
+
+}
